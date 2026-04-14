5c8038cf22cb2e8505a29f4d5065aad1d91a4e6f
saranovvv <mjurgas.szkola@gmail.com>
2026-04-14 14:34:36 +0200

    opracowanie onclick content long

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
-function test(){
-    print("test")
+function display(e){
+        
+    if(e.target.classList.contains("test")){
+        const content_long = document.getElementById('show-tekst')
+        content_long.classList.add('show')
+    }
 }
+  
+
+document.getElementById('show').addEventListener("click", e => {
+    display(e);
+})
